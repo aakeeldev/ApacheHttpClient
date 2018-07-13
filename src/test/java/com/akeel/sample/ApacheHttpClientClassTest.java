60587dd502858089ac4be286c244078dc655eb33
abdul akeel <aakeel@aaalife.com>
2018-07-13 12:00:28 -0400

    added fake test for bamboo testing

---
+++ src/test/java/com/akeel/sample/ApacheHttpClientClassTest.java
@@ -0,0 +1,19 @@
+package com.akeel.sample;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApacheHttpClientClassTest {
+	
+	@Before
+	public void setup() {
+		//sample code here for the initialization
+		
+	}
+	
+	@Test
+	public void testHttpMetthod() {
+		
+	}
+
+}
